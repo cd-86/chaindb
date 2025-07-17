41339d9a0a7051dc62d1ecab39f91e87fcf509d1
shynur <shynur@outlook.com>
2025-07-17 19:45:20 +0800

    ;

--- blockchain/a.go
+++ blockchain/a.go
@@ -1,5 +1,7 @@
 package blockchain
 
+import "sync"
+
 type Transaction struct {
 	OwnerID uint32 `json:"OwnerID"`
 	Nonce   uint32 `json:"Nonce"`
@@ -11,4 +13,17 @@ type Block struct {
 	UUID         uint32        `json:"UUID"`
 	ParentUUID   uint32        `json:"ParentUUID"`
 	Transactions []Transaction `json:"Transactions"`
+}
+
+type Chain struct {
+	blocks []Block
+	lock   sync.RWMutex
+}
+
+func NewChain() *Chain {
+	var chain Chain
+	chain.blocks = []Block{
+		//TODO {Timestamp: }
+	}
+	return &chain
 }
