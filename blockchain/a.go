c7945e9cef93afda5a1ae57f77ccdb728929edbb
shynur <shynur@outlook.com>
2025-07-17 19:29:07 +0800

    ;

--- blockchain/a.go
+++ blockchain/a.go
@@ -1,9 +1,15 @@
 package blockchain
 
-type Transaction struct{}
+type Transaction struct {
+	Timestamp float64 `json:"Timestamp"`
+	OwnerID   uint32  `json:"OwnerID"`
+	Nonce     uint32  `json:"Nonce"`
+	Data      string  `json:"Data"`
+}
 
 type Block struct {
-	Timestamp  float64 `json:"Timestamp"`
-	UUID       uint32  `json:"UUID"`
-	ParentUUID uint32  `json:"ParentUUID"`
+	Timestamp    float64       `json:"Timestamp"`
+	UUID         uint32        `json:"UUID"`
+	ParentUUID   uint32        `json:"ParentUUID"`
+	Transactions []Transaction `json:"Transactions"`
 }
