5eb862d18773c837cb0ba51d3571bf1ad1f3c39e
shynur <shynur@outlook.com>
2025-07-18 16:11:28 +0800

    ;

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"time"
 
 	"github.com/shynur/chaindb/discovery"
 )
 
 func main() {
-	discovery.Register(10 * time.Second)
-	discovery.FindAll(5 * time.Second)
+	discovery.Start(1 * time.Second)
+	for ; ; time.Sleep(1 * time.Second) {
+		for _, ip_addr := range *discovery.ActiveNodes.Load() {
+			fmt.Println(ip_addr)
+		}
+		fmt.Println()
+	}
 }
