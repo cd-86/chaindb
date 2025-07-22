2bc5b782fa210c09e873ec54cd89624d32eb7ce3
shynur <shynur@outlook.com>
2025-07-22 20:43:14 +0800

    ;

--- cmd/main.go
+++ cmd/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"math/rand/v2"
+	"time"
+
 	"github.com/shynur/chaindb/blockchain"
 	chaindb_config "github.com/shynur/chaindb/config"
 	"github.com/shynur/chaindb/discovery"
@@ -10,5 +13,11 @@ func main() {
 	discovery.Start(chaindb_config.DiscoveryInterval)
 	blockchain.StartMining(LocalChain, &LocalTxPool)
 
-	select {}
+	for ; ; time.Sleep(100 * time.Millisecond) {
+		tx := blockchain.Transaction{
+			OwnerID: rand.Uint32N(5),
+			Nonce:   rand.Uint32N(10),
+		}
+		LocalTxPool.Add(tx)
+	}
 }
