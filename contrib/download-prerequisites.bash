5274b3d628de807abcf9bfca8dee4a4fa99b1940
shynur <shynur@outlook.com>
2025-07-24 15:07:02 +0800

    ;

--- contrib/download-prerequisites.bash
+++ contrib/download-prerequisites.bash
@@ -5,14 +5,21 @@ if [ linux-gnu != $OSTYPE ]; then
     exit 1
 fi
 
-# 安装 Go 工具链.
-sudo rm -rf /usr/local/go
 cd /tmp
 mkdir -p shynur/Downloads
 cd shynur/Downloads
+
+# 安装 Go 工具链.
+sudo rm -rf /usr/local/go
 wget https://go.dev/dl/go1.24.5.linux-amd64.tar.gz
 sudo tar -C /usr/local -xzf go1.24.5.linux-amd64.tar.gz
 export PATH+=:/usr/local/go/bin
 go version
 
-# 安装 gRPC 及其依赖.
+# 安装 protoc.
+curl -LO  \
+    https://github.com/protocolbuffers/protobuf/releases/download/v30.2/protoc-30.2-linux-x86_64.zip
+mkdir -p ~/.local
+unzip protoc-30.2-linux-x86_64.zip -d $HOME/.local
+export PATH+=:~/.local/bin
+protoc --version
