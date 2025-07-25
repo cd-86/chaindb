3fb0afdd00f1d100f5827e99f4a54d70158e792e
shynur <shynur@outlook.com>
2025-07-25 15:52:02 +0800

    ;

--- contrib/sync2cnb.bash
+++ contrib/sync2cnb.bash
@@ -7,5 +7,8 @@ mkdir -p /tmp/shynur
 cp -r . /tmp/shynur/chaindb.git
 
 cd /tmp/shynur/chaindb.git
+ln -s ./docs/ReadMe.md ReadMe.md
+git add ReadMe.md
+git commit -m '为 CNB 添加仓库及 ReadMe 文件'
 git remote set-url origin https://cnb.cool/shynur/chaindb.git
 git push -f
