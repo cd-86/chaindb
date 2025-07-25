9d59da70541204fb15230ca58656864464d42887
shynur <shynur@outlook.com>
2025-07-25 20:48:59 +0800

    ;

--- contrib/download-prerequisites.bash
+++ contrib/download-prerequisites.bash
@@ -10,24 +10,29 @@ mkdir -p shynur/Downloads
 cd shynur/Downloads
 
 # 安装 Go 工具链.
-sudo rm -rf /usr/local/go
-precompiled_go=go1.24.5.linux-amd64.tar.gz
-if ! [ -f $precompiled_go ]; then
-    wget https://go.dev/dl/$precompiled_go
-fi
-sudo tar -C /usr/local -xzf $precompiled_go
+go_version=1.24.5
 export PATH+=:/usr/local/go/bin
+if ! go version | grep -F $go_version >/dev/null; then
+    sudo rm -rf /usr/local/go
+    precompiled_go=go$go_version.linux-amd64.tar.gz
+    if ! [ -f $precompiled_go ]; then
+        wget https://go.dev/dl/$precompiled_go
+    fi
+    sudo tar -C /usr/local -xzf $precompiled_go
+fi
 go version
 
 # 安装 protoc.
 protoc_version=30.2
-if ! [ -f protoc-$protoc_version-linux-x86_64.zip ]; then
-    curl -LO  \
-        https://github.com/protocolbuffers/protobuf/releases/download/v$protoc_version/protoc-$protoc_version-linux-x86_64.zip
-fi
 mkdir -p ~/.local
-unzip -o protoc-$protoc_version-linux-x86_64.zip -d $HOME/.local
 export PATH+=:~/.local/bin
+if ! protoc --version | grep -F $protoc_version >/dev/null; then
+    if ! [ -f protoc-$protoc_version-linux-x86_64.zip ]; then
+        curl -LO  \
+            https://github.com/protocolbuffers/protobuf/releases/download/v$protoc_version/protoc-$protoc_version-linux-x86_64.zip
+    fi
+    unzip -o protoc-$protoc_version-linux-x86_64.zip -d $HOME/.local
+fi
 protoc --version
 
 # 安装 gRPC.
