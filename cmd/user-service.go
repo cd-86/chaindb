773d0e02f6d14aa9459b36182196974bb9fedd92
shynur <shynur@outlook.com>
2025-07-30 11:00:32 +0800

    ;

--- cmd/user-service.go
+++ cmd/user-service.go
@@ -1,7 +1,29 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net"
+	"net/http"
+	"strconv"
+
+	"github.com/shynur/chaindb/chaindb_config"
+)
 
 func StartUserService() {
 	server := http.NewServeMux()
+
+	server.HandleFunc("/api/v1", func(w http.ResponseWriter, r *http.Request) {
+
+	})
+
+	go func() {
+		err := http.ListenAndServe(
+			net.JoinHostPort(
+				"",
+				strconv.Itoa(chaindb_config.TCPPortUserService),
+			),
+			server,
+		)
+		log.Fatalln("[UserService] Error:", err)
+	}()
 }
