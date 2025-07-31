e76db259b13ac346e65f6f608e82cf538cd3bcf4
shynur <shynur@outlook.com>
2025-07-31 07:12:36 +0000

    ;

--- user-client/chaindb.hpp
+++ user-client/chaindb.hpp
@@ -2,11 +2,27 @@
 #include <bits/stdc++.h>
 using namespace std::literals;
 
-namespace ::shynur::chaindb {
+namespace shynur::chaindb {
 
     struct [[gnu::weak]] Transaction {
+        /**
+         * @brief 数据条目在 ChainDB 中的索引号.
+         * @details 因为一个索引号对应的数据条目可能有很多个修订历史,
+         *          即一对多的关系, 所以称索引号为 owner.
+         * @warning 只能是自然数, 即 [0, 2**31).  其余范围供内部使用.
+         */
         const std::int32_t OwnerID;
+
+        /**
+         * @brief 修订历史, 表示这是第几次修订.
+         * @note 某个 owner 第一次插入数据, 该字段为 0.
+         * @details
+         */
         const unsigned Nonce;
+
+        /**
+         * @brief 数据条目的主体.
+         */
         const std::string Data;
     };
 
@@ -23,7 +39,7 @@ namespace ::shynur::chaindb {
                 const std::int32_t OwnerID;
 				const unsigned ConfirmationScore;
             };
-            auto owners = std::vector<owner>{};
+            auto owners = std::vector<Owner>{};
 
 
             return owners;
@@ -47,7 +63,7 @@ namespace ::shynur::chaindb {
 }
 
 #ifdef SEER_ROBOTICS_RBK
-namespace ::rbk::chaindb {
+namespace rbk::chaindb {
     using UserClient = ::shynur::chaindb::UserClient;
 }
 #endif
