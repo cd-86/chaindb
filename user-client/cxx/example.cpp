dcd260fa35ab9beeeb763743b983282dc03a6694
shynur <shynur@outlook.com>
2025-08-01 16:11:54 +0800

    ;

--- user-client/cxx/example.cpp
+++ user-client/cxx/example.cpp
@@ -5,6 +5,8 @@ namespace chaindb = rbk::chaindb;
 int main() {
     const auto uc = chaindb::UserClient{};
 
+    std::cout << std::endl;
+
     for (char op; std::cin; std::cout << std::endl) {
         std::cout << "Operation (o=列出索引, t=列出数据, i=插入数据): ";
         std::cin >> op;
@@ -13,6 +15,7 @@ int main() {
                 case 'o': {
                     std::cout << "(默认是 0) Confirmation Score >= ";
                     const auto confirmation = [] -> unsigned {
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                         auto confirmation = std::string{};
                         std::getline(std::cin, confirmation);
                         if (confirmation.empty())
@@ -32,6 +35,7 @@ int main() {
 
                     std::cout << "(默认是 0) Confirmation Score >= ";
                     const auto confirmation = [] -> unsigned {
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                         auto confirmation = std::string{};
                         std::getline(std::cin, confirmation);
                         if (confirmation.empty())
@@ -64,6 +68,7 @@ int main() {
 
                     std::cout << "(默认是 0) Confirmation Score >= ";
                     const auto confirmation = [] -> unsigned {
+                        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
                         auto confirmation = std::string{};
                         std::getline(std::cin, confirmation);
                         if (confirmation.empty())
