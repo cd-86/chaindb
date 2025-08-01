631537776ceb85115d6147136d7ac5862ad18f90
shynur <shynur@outlook.com>
2025-08-01 15:56:19 +0800

    ;

--- cmd/main.go
+++ cmd/main.go
@@ -13,7 +13,9 @@ func main() {
 
 	validator.StartBlockDeliveryServer()
 	validator.StartTryPickBlocks(LocalChain)
+
 	blockchain.StartMining(LocalChain, &LocalTxPool, validator.Propose)
+
 	monitor.Start(LocalChain, &LocalTxPool)
 	StartUserService()
 
