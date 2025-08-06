50233a4d9936f91af237e544451f07ad0cdb5451
shynur <shynur@outlook.com>
2025-08-06 11:37:54 +0800

    ; Update download-prerequisites.bash

--- contrib/download-prerequisites.bash
+++ contrib/download-prerequisites.bash
@@ -21,7 +21,7 @@ cd shynur/Downloads
 
 # 安装 Go 工具链.
 export PATH+=:/usr/local/go/bin
-if ! go version | grep -F $go_version >/dev/null; then
+if ! go version | grep -F $go_version &>/dev/null; then
     sudo rm -rf /usr/local/go
     precompiled_go=go$go_version.linux-amd64.tar.gz
     if ! [ -f $precompiled_go ]; then
@@ -34,7 +34,7 @@ go version
 # 安装 protoc.
 mkdir -p ~/.local
 export PATH+=:~/.local/bin
-if ! protoc --version | grep -F $protoc_version >/dev/null; then
+if ! protoc --version | grep -F $protoc_version &>/dev/null; then
     if ! [ -f protoc-$protoc_version-linux-x86_64.zip ]; then
         curl -LO  \
             https://github.com/protocolbuffers/protobuf/releases/download/v$protoc_version/protoc-$protoc_version-linux-x86_64.zip
