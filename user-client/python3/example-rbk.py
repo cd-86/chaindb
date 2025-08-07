e0bf84dbed3b7740214c3d7854917a5db0ffa717
shynur <shynur@outlook.com>
2025-08-07 17:31:31 +0800

    ;

---
+++ user-client/python3/example-rbk.py
@@ -0,0 +1,64 @@
+#! /bin/python3
+
+from . import chaindb
+
+RequiredConfirmations: int = 10
+# 表示区块链节点间相互同步的数据的可靠性.
+# 可靠性会导致更高的延迟.  基本上
+#     延迟 = RequiredConfirmations * 1.25 seconds
+# 该变量可配置, 建议所有节点使用相同的 RequiredConfirmations.
+
+uc = chaindb.UserClient()
+
+rbk_available_owners = range(100_0000, 200_0000)
+
+
+def Owners() -> list[int]:
+    """
+    获取 ChainDB 里的 keys (也称 owners).
+    """
+    owners = uc.ListOwners(confirmations=RequiredConfirmations)
+    return [owner.OwnerID for owner in owners if owner.OwnerID in rbk_available_owners]
+
+
+def Get(owner: int) -> list[str]:
+    """
+    按 owner 获取 value, value 是一个数组, 按插入时间的顺序排列.
+    """
+    txs = uc.ListTransactionsOwnedBy(owner, confirmations=RequiredConfirmations)
+    txs.sort(key=lambda tx: tx.Transaction.Nonce)
+    return [tx.Transaction.Data for tx in txs]
+
+
+def Insert(owner: int, data: str) -> int:
+    """
+    向 ChainDB 插入数据.
+    也即, 在 owner 映射的数组后追加一个 data.
+
+    新建键值对 (owner -> [data, ...]) 时, 请用随机数作为 owner, 避免冲突.
+
+    返回被插入的数据在 owner 的数组中的索引.
+    """
+    owner = (
+        owner % (rbk_available_owners.stop - rbk_available_owners.start)
+        + rbk_available_owners.start
+    )
+
+    nonce = len(Get(owner))
+
+    while True:
+        ok = uc.Insert(
+            chaindb.blockchain.Transaction(
+                OwnerID=owner,
+                Nonce=nonce,
+                Data=data,
+            ),
+            confirmations=RequiredConfirmations,
+        )
+        if ok:
+            return nonce
+
+        if len(Get(owner)) > nonce:
+            if Get(owner)[nonce] == data:
+                return nonce
+            return Insert(owner, data)
